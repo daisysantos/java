74fe2e9fd733c8dd408d99d5534fe67aabb882ba
Daisy Santos <daisy.santos@sumup.com>
2023-03-23 18:48:22 -0300

    Exercicio Aula06

--- Reforco/src/Exercicio.java
+++ Reforco/src/Exercicio.java
@@ -13,9 +13,16 @@ public class Exercicio {
 		double price2 = 650.50;
 		double measure = 53.234567;
 		
-		System.out.printf("Produtcts: ");
-		
-		System.out.printf("Record: " + age + code);
+		System.out.printf("Products:");
+		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+		System.out.println();
+		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+		System.out.println();
+		System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f%n", measure);
 		
 		
 	}
